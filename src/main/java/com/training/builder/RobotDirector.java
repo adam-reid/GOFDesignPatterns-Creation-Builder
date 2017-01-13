7e7402157da53da346b9d6d57bb21f8aee62e4b3
Adam Reid <adameastcoast@gmail.com>
2017-01-13 00:58:06 -0500

    RobotDirector class set up

--- src/main/java/com/training/builder/RobotDirector.java
+++ src/main/java/com/training/builder/RobotDirector.java
@@ -4,4 +4,22 @@ package com.training.builder;
  * Created by adame on 1/13/2017.
  */
 public class RobotDirector {
+    private RobotBuilder robotBuilder;
+
+    public RobotDirector(RobotBuilder robotBuilder) {
+        this.robotBuilder = robotBuilder;
+    }
+
+    public void makeRobot() {
+        this.robotBuilder.buildRobotHead();
+        this.robotBuilder.buildRobotArms();
+        this.robotBuilder.buildRobotTorso();
+        this.robotBuilder.buildRobotLegs();
+        this.robotBuilder.buildRobotCatchPhrase();
+        this.robotBuilder.buildRobotAction();
+    }
+
+    public Robot getRobot() {
+        return this.robotBuilder.getRobot();
+    }
 }
