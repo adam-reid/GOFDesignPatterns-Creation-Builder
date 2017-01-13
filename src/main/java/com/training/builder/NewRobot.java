554aed183ee40cb0caea19b7c3aebdfd97d05bec
Adam Reid <adameastcoast@gmail.com>
2017-01-13 00:53:01 -0500

    Create Old builder; remove extra classes

--- src/main/java/com/training/builder/NewRobot.java
+++
@@ -1,7 +0,0 @@
-package com.training.builder;
-
-/**
- * Created by adame on 1/13/2017.
- */
-public class NewRobot {
-}
--- src/main/java/com/training/builder/OldRobotBuilder.java
+++ src/main/java/com/training/builder/OldRobotBuilder.java
@@ -3,5 +3,46 @@ package com.training.builder;
 /**
  * Created by adame on 1/13/2017.
  */
-public class OldRobotBuilder {
+public class OldRobotBuilder implements RobotBuilder {
+
+    private Robot robot;
+
+    public OldRobotBuilder(Robot robot) {
+        this.robot = robot;
+    }
+
+    @Override
+    public void buildRobotHead() {
+        robot.setRobotHead("TV head");
+    }
+
+    @Override
+    public void buildRobotArms() {
+        robot.setRobotArms("Chainsaw arms");
+    }
+
+    @Override
+    public void buildRobotTorso() {
+        robot.setRobotTorso("Trash can torso");
+    }
+
+    @Override
+    public void buildRobotLegs() {
+        robot.setRobotLegs("Tire legs");
+    }
+
+    @Override
+    public void buildRobotCatchPhrase() {
+        robot.getRobotCatchPhrase("Gee golly!");
+    }
+
+    @Override
+    public void buildRobotAction() {
+        robot.setRobotAction("wheelie head stand action!");
+    }
+
+    @Override
+    public Robot getRobot() {
+        return null;
+    }
 }
