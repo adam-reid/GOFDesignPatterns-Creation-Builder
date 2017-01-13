c30860d3d4f126c5d320a5e60be1938b2670c439
Adam Reid <adameastcoast@gmail.com>
2017-01-13 01:10:56 -0500

    Refactor main test; add new robot

--- src/main/java/com/training/builder/main.java
+++ src/main/java/com/training/builder/main.java
@@ -1,16 +1,23 @@
 package com.training.builder;
 
+import sun.security.x509.RDN;
+
 /**
  * Created by adame on 1/13/2017.
  */
 public class main {
-    public static void main(String[] args) {
-        RobotBuilder oldRobotPlans = new OldRobotBuilder();
-        RobotDirector robotDirector = new RobotDirector(oldRobotPlans);
+
+    public static void testRobot(RobotBuilder blueprints) {
+        RobotDirector robotDirector = new RobotDirector(blueprints);
 
         robotDirector.makeRobot();
         Robot robot = robotDirector.getRobot();
 
         System.out.println(robot.toString());
+    }
+
+    public static void main(String[] args) {
+        testRobot(new OldRobotBuilder());
+        testRobot(new NewRobotBuilder());
     }
 }
