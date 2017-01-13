1abd8832eecb446fb38385c17c036275d437af67
Adam Reid <adameastcoast@gmail.com>
2017-01-13 01:07:51 -0500

    Add a new robot!

--- src/main/java/com/training/builder/NewRobotBuilder.java
+++ src/main/java/com/training/builder/NewRobotBuilder.java
@@ -3,5 +3,46 @@ package com.training.builder;
 /**
  * Created by adame on 1/13/2017.
  */
-public class NewRobotBuilder {
+public class NewRobotBuilder implements RobotBuilder {
+
+    private Robot robot;
+
+    public NewRobotBuilder() {
+        this.robot = new Robot();
+    }
+
+    @Override
+    public void buildRobotHead() {
+        this.robot.setRobotHead("LCD screen");
+    }
+
+    @Override
+    public void buildRobotArms() {
+        this.robot.setRobotArms("lasers!");
+    }
+
+    @Override
+    public void buildRobotTorso() {
+        this.robot.setRobotTorso("flux capacitor");
+    }
+
+    @Override
+    public void buildRobotLegs() {
+        this.robot.setRobotLegs("hoverboards");
+    }
+
+    @Override
+    public void buildRobotCatchPhrase() {
+        this.robot.setRobotCatchPhrase("I'm so 2008. You so 2000 and late.");
+    }
+
+    @Override
+    public void buildRobotAction() {
+        this.robot.setRobotAction("karate chop action!");
+    }
+
+    @Override
+    public Robot getRobot() {
+        return this.robot;
+    }
 }
