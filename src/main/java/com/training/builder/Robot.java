bdebbb2177c62a14e296219152cfe6ca5d84571b
Adam Reid <adameastcoast@gmail.com>
2017-01-13 00:38:24 -0500

    Set up RobotBuilder interface

--- src/main/java/com/training/builder/Robot.java
+++ src/main/java/com/training/builder/Robot.java
@@ -4,4 +4,5 @@ package com.training.builder;
  * Created by adame on 1/13/2017.
  */
 public interface Robot {
+    void
 }
--- src/main/java/com/training/builder/RobotBuilder.java
+++ src/main/java/com/training/builder/RobotBuilder.java
@@ -4,4 +4,11 @@ package com.training.builder;
  * Created by adame on 1/13/2017.
  */
 public interface RobotBuilder {
+    void buildRobotHead();
+    void buildRobotArms();
+    void buildRobotTorso();
+    void buildRobotLegs();
+    void buildRobotCatchPhrase();
+    void buildRobotAction();
+    Robot getRobot();
 }
