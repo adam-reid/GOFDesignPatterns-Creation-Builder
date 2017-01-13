a5e2d94881164faee8b657ac2ed24c4d166d7832
Adam Reid <adameastcoast@gmail.com>
2017-01-13 00:34:50 -0500

    Update main template; robot is interface

--- src/main/java/com/training/builder/Robot.java
+++ src/main/java/com/training/builder/Robot.java
@@ -3,5 +3,5 @@ package com.training.builder;
 /**
  * Created by adame on 1/13/2017.
  */
-public class Robot {
+public interface Robot {
 }
--- src/main/java/com/training/builder/main.java
+++ src/main/java/com/training/builder/main.java
@@ -4,4 +4,9 @@ package com.training.builder;
  * Created by adame on 1/13/2017.
  */
 public class main {
+    public static void main(String[] args) {
+        RobotBuilder robotBuilder;
+        RobotDirector robotDirector;
+        Robot robot;
+    }
 }
